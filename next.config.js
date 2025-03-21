5c0774e463c6adbd6fa7d4e9853cfeb2ed053253
Nils De Rijke <nilsderijke@Nilss-MacBook-Pro.local>
2025-03-21 21:28:57 +0200

    fix: update next.config.js for proper build optimization

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'standalone',
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 module.exports = nextConfig;
